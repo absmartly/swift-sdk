58b40170318b13b4f530ab1e8c2b3f6d0e6ecad1
Roman <roman.odyshew@quantumsoft.ru>
2021-09-16 19:47:26 +0700

    add fixes for macOS

--- Sources/Logger.swift
+++ Sources/Logger.swift
@@ -10,31 +10,48 @@ import OSLog
 
 class Logger {
     static func error(_ error: String) {
+        #if os(OSX)
+        if #available(macOS 11.0, *) {
+            let customLog = os.Logger(subsystem: "ABSmartly", category: "")
+            customLog.error("\(error)")
+        } else {
+            let log = OSLog(subsystem: "ABSmartly", category: "")
+            os_log("%@", log: log, type: .error, error)
+        }
+        
+        #else
         if #available(iOS 14.0, *) {
             let customLog = os.Logger(subsystem: "ABSmartly", category: "")
             customLog.error("\(error)")
-            
         } else if #available(iOS 10.0, *) {
-            
             let log = OSLog(subsystem: "ABSmartly", category: "")
             os_log("%@", log: log, type: .error, error)
-            
         } else {
             print("ABSmartly Error: " + error)
         }
+        #endif
     }
     
     static func notice(_ note: String) {
+        #if os(OSX)
+        if #available(macOS 11.0, *) {
+            let customLog = os.Logger(subsystem: "ABSmartly", category: "")
+            customLog.notice("\(note)")
+        } else {
+            let log = OSLog(subsystem: "ABSmartly", category: "")
+            os_log("%@", log: log, type: .default, note)
+        }
+        
+        #else
         if #available(iOS 14.0, *) {
             let customLog = os.Logger(subsystem: "ABSmartly", category: "")
             customLog.notice("\(note)")
         } else if #available(iOS 10.0, *) {
-            
             let log = OSLog(subsystem: "ABSmartly", category: "")
             os_log("%@", log: log, type: .default, note)
-            
         } else {
             print("ABSmartly Note: " + note)
         }
+        #endif
     }
 }
